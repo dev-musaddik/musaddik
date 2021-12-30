6f9549a16570303cf62d3e0cfa29e04ac2950d47
Md. Musaddik Hosen <96862282+dev-musaddik@users.noreply.github.com>
2021-12-30 21:32:34 +0600

    Add files via upload

---
+++ website.mh.js
@@ -0,0 +1,11 @@
+show = document.getElementById('zaki')
+
+function openmu() {
+    show.style.right = "0";
+    show.style.background = "palevioletred";
+}
+
+function closemu() {
+    show.style.right = "-150px"
+    show.style.background = "black";
+}
